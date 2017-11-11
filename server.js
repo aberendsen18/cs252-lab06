6b63a2c07ca55e1a6bddb5324b6c0b388cb93d6d
Ramsey Ali <ali59@purdue.edu>
2017-11-11 18:55:37 -0500

    Basic server

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http')
+
+var server = http.createServer((function(request,response)
+
+    {
+      response.writeHead(200,
+
+        {"Content-Type": "text/plain"});
+
+      response.end("Hello world\n");
+
+    }));
+
+
+server.listen(7000);

43c82b38d8b4bba9dc16e56a97229bbdc7c4efdb
Ramsey Ali <ali59@purdue.edu>
2017-12-04 15:07:35 -0500

    Login functionality
    * Login page added
    * Registration page added
    * Framework added for lobby work
    * Framework added for front-end design work

--- nodetest2/routes/index.js
+++ nodetest2/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Login' });
+});
+
+/* GET index page. */
+router.get('/lobby', function(req, res, next) {
+  res.render('lobby', { title: 'Lobby' });
+});
+
+/* GET create account page. */
+router.get('/create', function(req, res, next) {
+  res.render('create', { title: 'Create Account' });
 });
 
 module.exports = router;
